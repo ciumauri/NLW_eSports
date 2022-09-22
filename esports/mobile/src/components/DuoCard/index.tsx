421e52ef45ca2e1c1a53fc20a18932bc23651ca3
Mauricio Oliveira <ciumauri@gmail.com>
2022-09-22 15:37:21 -0300

    Finalizando Tela Game/GameCard

--- esports/mobile/src/components/DuoCard/index.tsx
+++ esports/mobile/src/components/DuoCard/index.tsx
@@ -1,27 +1,60 @@
-import { View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { GameController } from 'phosphor-react-native'
+
 import { DuoInfo } from '../DuoInfo';
 
 import { styles } from './styles';
+import { THEME } from '../../theme';
 
-export function DuoCard() {
+export interface DuoCardProps {
+  hourEnd: string;
+  hourStart: string;
+  id: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+}
+
+interface Props {
+  data: DuoCardProps;
+  onConect: () => void;
+}
+
+export function DuoCard({ data, onConect }: Props) {
   return (
     <View style={styles.container}>
       <DuoInfo
-        label='Nomes'
-        value='Mauricio Oliveira'
+        label='Nome'
+        value={data.name}
       />
       <DuoInfo
-        label='Nome'
-        value='Mauricio Oliveira'
+        label='Tempo de jogo'
+        value={`${data.yearsPlaying} ano(s)`}
       />
       <DuoInfo
-        label='Nome'
-        value='Mauricio Oliveira'
+        label='Disponibilidade'
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
       />
       <DuoInfo
-        label='Nome'
-        value='Mauricio Oliveira'
+        label='Chamada de áudio?'
+        value={data.useVoiceChannel ? 'Sim' : 'Não'}
+        colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
       />
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={onConect}
+      >
+        <GameController
+          color={THEME.COLORS.TEXT}
+          size={20}
+        />
+
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>        
+      </TouchableOpacity>
     </View>
   );
 }
